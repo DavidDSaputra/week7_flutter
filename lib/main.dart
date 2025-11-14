333a858edd830185fd90933f92dbab0ab26ccdf9
DavidDSaputra <davidsaputra2704@gmail.com>
2025-11-14 20:54:10 +0700

    test roboto bold

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ const MyApp({super.key});
 Widget build(BuildContext context) {
 return MaterialApp(
 title: 'Custom Fonts',
-// Set Raleway as the default app font.
+
 theme: ThemeData(fontFamily: 'Raleway'),
 home: const MyHomePage(),
 );
@@ -17,13 +17,13 @@ const MyHomePage({super.key});
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-// The AppBar uses the app-default Raleway font.
+
 appBar: AppBar(title: const Text('Custom Fonts')),
 body: const Center(
-// This Text widget uses the RobotoMono font.
+
 child: Text(
 'Roboto Mono sample',
-style: TextStyle(fontFamily: 'RobotoMono'),
+style: TextStyle(fontFamily: 'Roboto Bold', fontSize: 24),
 ),
 ),
 );
